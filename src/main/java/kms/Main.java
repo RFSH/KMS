8cc02d63f07e8354f881fc077a0d327b1e8d86af
ha-D <hadi.zolfaghaari@gmail.com>
2015-05-13 16:12:06 +0430

    Add gradle files

---
+++ src/main/java/kms/Main.java
@@ -0,0 +1,29 @@
+package kms;
+
+import javafx.event.EventHandler;
+import javafx.scene.web.WebEvent;
+import javang.JavaNGWindow;
+
+public class Main {
+	public static void main(String[] args) {
+		String indexUrl = Main.class.getClassLoader().getResource("index.html").toExternalForm();
+//		indexUrl = "http://localhost:9000/";
+		new JavaNGWindow(indexUrl, 800, 600, new JavaNGWindow.StateListener() {
+			@Override
+			public void onStageInit(JavaNGWindow window) {
+				window.getWebEngine().onAlertProperty().set(new EventHandler<WebEvent<String>>() {
+					@Override
+					public void handle(WebEvent<String> event) {
+						System.out.println("ALEERT " + event.getData());
+					}
+				});
+			}
+
+			@Override
+			public void onReady(JavaNGWindow window) {
+				System.out.println("READY");
+
+			}
+		});
+	}
+}
